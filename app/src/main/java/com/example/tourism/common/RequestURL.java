5567b5ac84e297ca85d4c5bebab69112e4983cfa
HBF <1012680470@qq.com>
2019-10-22 17:13:55 +0800

    实现弹幕效果

--- app/src/main/java/com/example/tourism/common/RequestURL.java
+++ app/src/main/java/com/example/tourism/common/RequestURL.java
@@ -3,5 +3,6 @@ package com.example.tourism.common;
 public class RequestURL {
     public static final String ip_port = "http://192.168.2.227:8080/api/";
     public static final String ip_images = "http://192.168.2.227:8080/";
+    public static final String ip_video = "http://192.168.2.227:8080/video";
 
 }
