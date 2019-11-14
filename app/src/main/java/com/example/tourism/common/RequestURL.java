41c2d5f3fcb202d8e95258bd9f0dffbfde0bc8ac
zp <123456@qq.com>
2019-11-14 21:43:45 +0800

    主页定位更新

--- app/src/main/java/com/example/tourism/common/RequestURL.java
+++ app/src/main/java/com/example/tourism/common/RequestURL.java
@@ -18,11 +18,11 @@ public class RequestURL {
     public static String vUserId = "";
 
     //IP地址,端口号
-    public static final String ip_port = "http://192.168.2.227:8080/api/";
+    public static final String ip_port = "http://47.100.47.54/TourismServer-1.0-SNAPSHOT/api/";
     //IP地址，视频路径
-    public static final String ip_video = "http://192.168.2.227:8080/video";
+    public static final String ip_video = "http://47.100.47.54/TourismServer-1.0-SNAPSHOT/video";
     //IP地址,图片路径
-    public static final String ip_images = "http://192.168.2.227:8080/";
+    public static final String ip_images = "http://47.100.47.54/TourismServer-1.0-SNAPSHOT/";
     //首页地址
     public static final String html = "https://travel.qunar.com/";
     //攻略库首页地址
